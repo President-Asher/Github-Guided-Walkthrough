a511bda252c22fa1972e7d9dc726e202153f552f
President-Asher <Asher.renzenbrink@gmail.com>
2023-09-27 08:25:27 -0400

    first commit

---
+++ Github_Guided_Walkthrough.py
@@ -0,0 +1,9 @@
+def spam():
+    print("WHY DONT YOU LIKE IT?!?!?!")
+    spam()
+print("Do you like my hair? Type Yes or No")
+type_1 = input("Type Here: ")
+if type_1 == 'Yes':
+    print("We are now friends forever.")
+elif type_1 == 'No':
+    spam()
